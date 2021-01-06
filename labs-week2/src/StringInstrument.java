279a6dc8ddff22b14369fa50794056adc397bad0
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-06 10:12:31 -0500

    Instrument information (derived classes)

---
+++ labs-week2/src/StringInstrument.java
@@ -0,0 +1,30 @@
+// TODO: Define a class: StringInstrument that is derived from the Instrument class
+public class StringInstrument extends Instrument {
+    
+    private int numStrings;
+    private int numFrets;
+
+    // TODO: Define mutator methods -
+    //      setNumOfStrings(), setNumOfFrets()
+
+    public void setNumOfStrings(int numStrings) {
+        this.numStrings = numStrings;
+    }
+
+    public void setNumOfFrets(int numFrets) {
+        this.numFrets = numFrets;
+    }
+
+    // TODO: Define accessor methods -
+    //      getNumOfStrings(), getNumOfFrets()
+
+    public int getNumOfStrings() {
+        return this.numStrings;
+    }
+
+    public int getNumOfFrets() {
+        return this.numFrets;
+    }
+
+}
+
